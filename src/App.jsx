ec36f6daa62b8d5f0233a1ec00608392ec281708
https://github.com/Samandar0021305 <samandar021325@gmail.com>
2022-07-30 11:00:18 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div >
       <Navbar />
-
+      <Header />
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,22 @@
 import "./hedaer.css";
 
+import Img from "../../img/IMAGE (11).png";
+
+
 function Header(){
     return(
-        <header>
+        <header className="header">
             <div className="container">
-                
+                <ul>
+                    <li>
+                        <h2>META RECORD LABEl</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                        <button className="header_btn">Learn more</button>
+                    </li>
+                    <li>
+                        <img src={Img} alt="" />
+                    </li>
+                </ul>
             </div>
         </header>
     )
